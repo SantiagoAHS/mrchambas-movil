e9610f6a6c63a1b208010c8e23d13b181c220bfc
SantiagoAHS <lionelsanti2018@gmail.com>
2025-10-24 17:21:36 -0600

    v1.1.1 mrchambas

---
+++ app/(tabs)/Inicio.tsx
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+export default function InicioScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Pantalla de Inicio 🏠</Text>
+      <Text>Aquí puedes ver la información general del usuario.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f0f8ff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
